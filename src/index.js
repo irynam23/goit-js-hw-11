aa48a3a7469854baacf3234dd252b67184ddc57c
Iryna <Iryna@MacBook-Air-MacBook.local>
2023-01-16 16:34:02 +0200

    upd2

--- src/index.js
+++ src/index.js
@@ -1,30 +1,59 @@
 import axios from 'axios';
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+let gallery = new SimpleLightbox('.gallery a');
 const refs = {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.load-more'),
 };
 
 const APIKEY = '32876010-953e9d7ee50a911a8b34edefd';
 const baseUrl = 'https://pixabay.com/api/';
 
 let query = '';
+let page = 1;
 
 refs.form.addEventListener('submit', onSubmit);
+refs.loadMore.addEventListener('click', onLoadMore);
 
 async function onSubmit(e) {
   e.preventDefault();
-
+  refs.gallery.innerHTML = '';
+  console.log(page);
   const searchQuery = e.target.elements.searchQuery.value;
   if (!searchQuery) {
     return;
   }
+  if (query && query !== searchQuery) {
+    page = 1;
+  }
   query = searchQuery;
-  const images = await getImages(searchQuery);
+  const images = await getImages(searchQuery, page);
+  if (images.totalHits) {
+    Notify.success(`Hooray! We found ${totalHits} images.`);
+  }
   renderImages(images.hits);
 }
-async function getImages(q) {
-  const response = await axios.get(`${baseUrl}?key=${APIKEY}&q=${q}`);
+async function getImages(q, pageNumber) {
+  const response = await axios.get(
+    `${baseUrl}?key=${APIKEY}&q=${q}&page=${pageNumber}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
+  );
+  const totalPages = Math.ceil(response.data.totalHits / 40);
+  if (!response.data.hits.length) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  if (totalPages > 1) {
+    refs.loadMore.classList.remove('is-hidden');
+  }
+  if (totalPages === page) {
+    refs.loadMore.classList.add('is-hidden');
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
   return response.data;
 }
 
@@ -41,19 +70,19 @@ function renderImages(imagesArray) {
         downloads,
       }) => {
         return `<div class="photo-card">
-  <img src="${webformatURL}" alt="" loading="lazy" />
+  <div class="image-wrap"><a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy"/></a></div>
   <div class="info">
     <p class="info-item">
-      <b>Likes: ${likes}</b>
+      <b>Likes</b> ${likes}
     </p>
     <p class="info-item">
-      <b>Views: ${views}</b>
+      <b>Views</b> ${views}
     </p>
     <p class="info-item">
-      <b>Comments: ${comments}</b>
+      <b>Comments</b> ${comments}
     </p>
     <p class="info-item">
-      <b>Downloads: ${downloads}</b>
+      <b>Downloads</b> ${downloads}
     </p>
   </div>
 </div>`;
@@ -61,4 +90,11 @@ function renderImages(imagesArray) {
     )
     .join('');
   refs.gallery.insertAdjacentHTML('beforeend', markup);
+  gallery.refresh();
+}
+
+async function onLoadMore() {
+  page += 1;
+  const images = await getImages(query, page);
+  renderImages(images.hits);
 }
